69434ab79c6e3888af24c977af1b54c5e82dde87
Haridas N <haridas.nss@gmail.com>
2021-06-04 09:05:57 +0530

    pulumi namespace:key setting validation.

--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/s3"
 	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/sqs"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
+
+type Infra struct {
+	Cpu int
+	Ram int
+}
+
+func GetInfra(ctx *pulumi.Context) *Infra {
+	var infra Infra
+	cfg := config.New(ctx, "us-west-1")
+	cfg.RequireObject("infra", &infra)
+	return &infra
+}
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Create an AWS resource (S3 Bucket)
 		regions := []string{"us-west-1"}
+
+		in := GetInfra(ctx)
+		fmt.Println("===> ", in.Cpu)
 
 		//provider, region, err := GetAWSProvider(ctx)
 		// if err != nil {
